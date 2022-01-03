1c11607db6d75867a43ea6560c2ebf872f0af160
Elminsterio <aljim91@gmail.com>
2022-01-03 20:29:02 +0100

    little errors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const generarEscrito = require('./modulos/plantilla');
 async function excutionCostas() {
 
     console.log('\n======================= Calculadora de Minutas =============================\n');
-    console.log('Esta aplicación realiza un cálculo esrictamente sobre los criterios ');
+    console.log('Esta aplicación realiza un cálculo estrictamente sobre los criterios ');
     console.log('de cada Colegio de Abogados, debiendo tener en cuenta el usuario de la');
     console.log('aplicación, la jurisprudencia en cada materia concreta a la hora de moderar');
     console.log('la minuta a los efectos de evitar impugnaciones.');
@@ -14,7 +14,7 @@ async function excutionCostas() {
     console.log('Nota: En algunos casos puede que no esté activa la API de INE y no se podrá');
     console.log('calcular el valor de referencia actualizado con el IPC, en esos casos deberá');
     console.log('actualizarse de forma manual.\n');
-    console.log('Pruebe a ejecutar de nuevo la aplicación en esos casos \n\n');
+    console.log('Pruebe a ejecutar de nuevo la aplicación en esos casos. \n\n');
 
     await inicioPreguntas().then((result) => {
 
